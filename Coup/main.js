4c8809318ffbfe70a465689e6412c2463e8cc6c6
Cole Horan <colehoran@gmail.com>
2020-08-24 14:46:37 -0600

    Update on initializing the game
    Worked on creating a deck and giving two cards out to two players

--- Coup/main.js
+++ Coup/main.js
@@ -27,10 +27,37 @@ class Deck {
     
 }
 
+class ChipPile{
+    constructor(){
+        this.count = count;
+    }
+}
+
 class Player {
     constructor(name) {
         this.playerName = name;
         this.playerCards = [];
         this.playerChips = playerChips;
+    }
+}
+
+class Board {
+    constructor() {
+        this.cardsInMiddle = [];
+        this.players = [];
+    }
+    //this will need to be changed for anywhere from 3-6 players
+    //for loops will be written to automatically distribute cards based on the amount of players as well as remove chips based on amount of players
+    start(playerOneName, playerTwoName) {
+        this.players.push(new Player(playerOneName));
+        this.players.push(new Player(playerTwoName));
+        let d = new Deck();
+        d.createDeck();
+        d.shuffleDeck(); 
+        this.players[0].playerCards = d.cards.splice(0, 2);
+        this.players[1].playerCards = d.cards.splice(0, 2);
+        this.cardsInMiddle = d;
+        this.players[0].playerChips = 2;
+        this.players[1].playerChips = 2;
     }
 }
