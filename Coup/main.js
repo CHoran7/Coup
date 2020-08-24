47e856ca633e787829b72521fbfc1ce1935282fc
Cole Horan <colehoran@gmail.com>
2020-08-24 13:59:48 -0600

    creating a deck

--- Coup/main.js
+++ Coup/main.js
@@ -1,6 +1,26 @@
-let card = {
-    type: "duke",
-    visible: "down"
-};
+class Card {
+    constructor(type, status) {
+        this.type = type;
+        this.status = status;
+    }
+}
 
-console.log(card);
+class Deck {
+    constructor() {
+        this.cards = [];    
+    }
+                       
+    createDeck() {
+        let types = ['duke', 'captain', 'ambassador', 'assassian', 'contessa'];
+        let status = ['dead', 'alive'];
+
+        for (let i = 0; i < 3; i++){
+            for (let j = 0; j < types.length; j++){
+                this.cards.push(new Card((types[i], 'alive')));
+            }
+        }
+    }
+shuffleDeck() {
+        
+    }
+}
